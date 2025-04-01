04cb4893eb07b119947e37dcf56167bed2b1a2ef
francis <clyderitchie@yahoo.com>
2025-04-01 14:10:14 -0700

    Homepage update"

---
+++ client/src/components/navBar.jsx
@@ -0,0 +1,15 @@
+function NavBar() {
+  return (
+    <>
+      <div className="top-0 px-1  bg-card rounded-b-lg shadow-lg mb-8">
+        <div className="p-5 flex justify-center items-center text-2xl">
+          <a href="/">
+            <h1 className="">Film by Clyde</h1>
+          </a>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default NavBar;
---
+++ client/src/pages/home.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import NavBar from '../components/navBar';
+
+
+
+function Home() {
+
+    return (
+        <>
+        <NavBar />
+        <div className='flex justify-center items-center'>
+            <h1 className=''> 
+                Hello welcome to the homePage
+            </h1>
+        </div>
+        </>
+    )
+}
+
+export default Home;
